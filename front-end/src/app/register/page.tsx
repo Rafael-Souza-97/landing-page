220df753fa5538ad19aa531e3f4b26b6f18d34e9
Rafael Souza <souza_rafael.97@outlook.com>
2023-07-09 12:37:17 -0300

    feat: register page

---
+++ front-end/src/app/register/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import InConstruction from '@/components/InConstruction';
+
+const Register = () => {
+  return (
+    <>
+      <InConstruction />
+    </>
+  );
+};
+
+export default Register;
