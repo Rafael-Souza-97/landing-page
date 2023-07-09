aa58d938972111316c54e23d11fdfb74c3c6059d
Rafael Souza <souza_rafael.97@outlook.com>
2023-07-09 19:55:16 -0300

    feat: forms page

---
+++ src/app/forms/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import InConstruction from '@/components/InConstruction';
+
+const Forms = () => {
+  return (
+    <>
+      <InConstruction />
+    </>
+  );
+};
+
+export default Forms;
