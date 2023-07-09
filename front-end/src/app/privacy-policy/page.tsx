5dab95a8ad99cbbc33c84e8f5a4761520290630b
Rafael Souza <souza_rafael.97@outlook.com>
2023-07-09 12:49:53 -0300

    feat: privacy-policy page

---
+++ front-end/src/app/privacy-policy/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import InConstruction from '@/components/InConstruction';
+
+const PrivacyPolicy = () => {
+  return (
+    <>
+      <InConstruction />
+    </>
+  );
+};
+
+export default PrivacyPolicy;
